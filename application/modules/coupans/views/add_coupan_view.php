5bd92b318746e66198bd9e7febdb46810c131b7b
Hatim <hatimd80@gmail.com>
2020-10-06 18:12:22 +0530

    added the fixed amount functionality in coupon

--- application/modules/coupans/views/add_coupan_view.php
+++ application/modules/coupans/views/add_coupan_view.php
@@ -132,7 +132,7 @@
           <label class="control-label col-md-3 col-sm-3 col-xs-12" for="first-name">Discount amount <!-- <span class="required">*</span> -->
           </label>
           <div class="col-md-6 col-sm-6 col-xs-12">
-            <input type="number" value="<?php echo set_value('FixedDiscount');?>" name="FixedDiscount" id="fixed"  required="true" class="form-control col-md-7 col-xs-12">
+            <input type="number" value="<?php echo set_value('fixedDiscount');?>" name="fixedDiscount" id="fixed"  required="true" class="form-control col-md-7 col-xs-12">
           </div>
         </div>
 
